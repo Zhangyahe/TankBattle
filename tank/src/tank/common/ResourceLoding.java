9476516070000edab6116f681d0eabdb29eeaa0e
雅核 <59279330+Zhangyahe@users.noreply.github.com>
2020-04-27 19:28:32 +0800

    添加子弹图片资源

--- tank/src/tank/common/ResourceLoding.java
+++ tank/src/tank/common/ResourceLoding.java
@@ -23,7 +23,7 @@ import javax.imageio.ImageIO;
     */
 
 public class ResourceLoding {
-   public static BufferedImage tankL,tankU,tankR,tankD;
+   public static BufferedImage tankL,tankU,tankR,tankD,bulletL,bulletU,bulletR,bulletD;
    
    static {
 	   try {
@@ -31,6 +31,12 @@ public class ResourceLoding {
 		tankU  = ImageIO.read(ResourceLoding.class.getClassLoader().getResourceAsStream("images/tankU.gif"));
 		tankR  = ImageIO.read(ResourceLoding.class.getClassLoader().getResourceAsStream("images/tankR.gif"));
 		tankD  = ImageIO.read(ResourceLoding.class.getClassLoader().getResourceAsStream("images/tankD.gif"));
+		
+		bulletL  = ImageIO.read(ResourceLoding.class.getClassLoader().getResourceAsStream("images/bulletL.gif"));
+		bulletU  = ImageIO.read(ResourceLoding.class.getClassLoader().getResourceAsStream("images/bulletU.gif"));
+		bulletR  = ImageIO.read(ResourceLoding.class.getClassLoader().getResourceAsStream("images/bulletR.gif"));
+		bulletD  = ImageIO.read(ResourceLoding.class.getClassLoader().getResourceAsStream("images/bulletD.gif"));
+		
 	} catch (IOException e) {
 		e.printStackTrace();
 	}
