6c42a552167b1c293756aa9ed274c74fd60610c3
雅核 <59279330+Zhangyahe@users.noreply.github.com>
2020-04-27 14:40:19 +0800

    去掉jdk11的module-info

--- tank/src/module-info.java
+++
@@ -1,4 +0,0 @@
-module tank {
-	requires java.desktop;
-	requires org.junit.jupiter.api;
-}
--- tank/src/test/ImageTest.java
+++ tank/src/test/ImageTest.java
@@ -3,7 +3,6 @@ package test;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
@@ -19,11 +18,11 @@ class ImageTest {
 //			BufferedImage image = ImageIO.read(new File("C:/work/javaprojects/Tank_60/src/images/bulletD.gif"));
 //			assertNotNull(image);
 			
-			BufferedImage image2 = ImageIO.read(ImageTest.class.getClassLoader().getResourceAsStream("images/bulletD.gif"));
+			BufferedImage image2 = ImageIO.read(this.getClass().getClassLoader().getResourceAsStream("images/bulletD.gif"));
 			//this.getClass()
 			assertNotNull(image2);
 		} catch (IOException e) {
-			e.printStackTrace();
+			e.getMessage();
 		}
 	}
 
