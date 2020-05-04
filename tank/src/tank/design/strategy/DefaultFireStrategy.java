4c9a0b1dcc2706050713457beca2abc9cee079a7
雅核 <59279330+Zhangyahe@users.noreply.github.com>
2020-05-04 09:20:14 +0800

    修改单例模式；由饿汉改为静态内部类实现

--- tank/src/tank/design/strategy/DefaultFireStrategy.java
+++ tank/src/tank/design/strategy/DefaultFireStrategy.java
@@ -24,23 +24,37 @@ import tank.util.Group;
  */
 
 public class DefaultFireStrategy implements FireStrategy {
-	private static final DefaultFireStrategy INSTANCE = new DefaultFireStrategy();
+	/**
+	 * 禁止外部创建一个新的实例 FireStrategyImp.
+	 *
+	 */
 
 	private DefaultFireStrategy() {
 	}
 
-	public static DefaultFireStrategy getInstance() {
-		return DefaultFireStrategy.INSTANCE;
+	private static class FireStrategyImpHanlder {
+		private static final DefaultFireStrategy INSTANCE = new DefaultFireStrategy();
 	}
 
+	public static DefaultFireStrategy getInstance() {
+		return FireStrategyImpHanlder.INSTANCE;
+	}
+
+	/*
+	 * (非 Javadoc)
+	 * 
+	 * 
+	 * @param t
+	 * 
+	 * @see tank.design.strategy.FireStrategy#fire(tank.dto.TankDto)
+	 */
 	@Override
 	public void fire(TankDto t) {
 		int bx = t.getX() + TankDto.WIDTH / 2 - BulletDto.WIDTH / 2;
 		int by = t.getY() + TankDto.HEIGHT / 2 - BulletDto.HEIGHT / 2;
 		new BulletDto(bx, by, t.getDir(), t.getTf(), t.getGroup());
 		if (t.getGroup() == Group.GOOD) {
-			new Thread(()-> new Audio("audio/tank_fire.wav").play()).start();
+			new Thread(() -> new Audio("audio/tank_fire.wav").play()).start();
 		}
 	}
-
 }
